f8bf6ddff63e2b086ebb251b122efbb86a776862
Clifford Marley <cliffmwale97@gmail.com>
2021-01-10 19:01:06 +0200

    Commit

--- www/js/files.jsx
+++ www/js/files.jsx
@@ -1,25 +1,64 @@
 let images_array = []
+let application_of_focus
 $(document).ready(()=>{
     $("#captureImageBtn").click(()=>{
         if($("#document_to_view").val() == ""){
             swal('Attention!', 'Please select the type of document you would like to capture!', 'error')
             return
         }
-        navigator.camera.getPicture(onSuccess,onFail,{
-            quality:50,
-            allowEdit:true,
-            sourceType: Camera.PictureSourceType.CAMERA,
-            destinationType:Camera.DestinationType.DATA_URL
-        });
+        $("#imageActual").click()
+        // navigator.camera.getPicture(onSuccess,onFail,{
+        //     quality:50,
+        //     allowEdit:true,
+        //     sourceType: Camera.PictureSourceType.CAMERA,
+        //     destinationType:Camera.DestinationType.DATA_URL
+        // });
     });
+
+    $("#imageActual").change(function(){
+        if (
+      this.value != null &&
+      this.value != "" &&
+      this.value != typeof undefined
+    ) {
+      readURL2(document.getElementById("imageActual"));
+    }
+    })
 
 })
 
+function readURL2(input) {
+    if (input.files.length > 0 && input.files.length <= 4) {
+      $.each(input.files, function (i, v) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          const payload = {
+            document_type: $("#document_to_view").val(),
+            subtitle:$("#document_subtitle").val(),
+            file:e.target.result
+          }
+          images_array.push(payload);
+          renderImages();
+          $("#imageActual").val('')
+          //$(`#${canvas}`).css("background-image", `url(${e.target.result})`).transition("jiggle");
+        };
+        
+        reader.readAsDataURL(input.files[i]);
+      });
+    }
+  }
+
+  function postImage(){
+
+  }
+
 function renderImages(){
     let imageDom = images_array.map(image=>{
-        return `<div style="background-image:url('${image.file}')" class="col-4 image_section img img-thumbnail"></div>`
+        return `<div style="background-image:url('${image.file}')" class="col-4 mx-auto image_section img img-thumbnail"></div>`
     })
+    
     imageDom = imageDom.join('')
+    console.log(imageDom)
     $("#image_canvas").html(imageDom)
 }
 
@@ -35,4 +74,43 @@ function onSuccess(Image){
 
 function onFail(err){
     swal('','Failed to pick up image!','error');
+}
+
+function saveImages(){
+  if(images_array.length > 0){
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        documents: images_array,
+        application: application_of_focus,
+      }),
+    };
+  
+    $("#capture_modal").addClass('loading')
+    fetch(`${baseURL}/documents`, options)
+      .then((res) => res.json())
+      .then((res) => {
+          $("#capture_modal").removeClass('loading')
+          swal("", res.message, res.status);
+          toast(res.message, res.status);
+          if(res.status == 'success'){
+            $("#closeModalBtn").click()
+            
+            images_array = [];
+          }
+      })
+      .catch((err) => {
+        $("#capture_modal").removeClass('loading')
+        swal(
+          "Connection Error!",
+          "Failed to upload captured documents!",
+          "error"
+        );
+      });
+  }else{
+    swal('Attention!', 'You are required to select image files first!', 'warning')
+  }
 }
